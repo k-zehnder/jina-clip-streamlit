279c958a3937bd0f458e2a473946229e2f73b177
Kevin Z <kjzehnder3@gmail.com>
2022-03-30 11:43:12 -0700

    refactor helpers in frontend

--- frontend/helpers.py
+++ frontend/helpers.py
@@ -1,7 +1,4 @@
-import os
-import cv2
-import imutils
-from imutils import paths
+import base64
 from docarray import DocumentArray, Document
 from docarray.array.sqlite import SqliteConfig
 from jina import DocumentArray, Document
@@ -10,7 +7,43 @@ from jina.types.request import Request
 from docarray import DocumentArray, Document
 from docarray.array.sqlite import SqliteConfig
 from PIL import Image
+import streamlit as st
 
+
+def sidebar_bg(side_bg):
+
+   side_bg_ext = 'png'
+
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+
+def set_bg_hack_url(url):
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url({url});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 
 def get_docs_from_sqlite(connection, table):
@@ -28,6 +61,15 @@ def get_embedded_da_from_img_files(images_path, num):
 def create_query_da(search_term):
     return DocumentArray(Document(text=search_term))
 
+def plot_search_results(resp: Request):
+    for doc in resp.docs:
+        print(f'Query text: {doc.text}')
+        print(f'Matches:')
+        print('-' * 10)
+        # doc.matches[:3].plot_image_sprites()
+        print([d.uri for d in doc.matches[:3]])
+        print()
+
 def get_client(port=12345, show_progress=True):
     c = Client(port=port)
     c.show_progress = show_progress
@@ -41,14 +83,6 @@ def search_by_text(query_text, verbose=False):
         show_results(q, results)
     return results
 
-def plot_search_results(resp: Request):
-    for doc in resp.docs:
-        print(f'Query text: {doc.text}')
-        print(f'Matches:')
-        print('-' * 10)
-        print([d.uri for d in doc.matches[:3]])
-        print()
-
 def show_results(query, results):
     print(f"query_text: {query[0].text}")
     for d in results:
@@ -59,5 +93,7 @@ def show_results(query, results):
 def resize_image(filename, resize_factor=2):
     image = Image.open(filename)
     w, h = image.size
-    image.resize((w * resize_factor, h * resize_factor), Image.ANTIALIAS)
+    image = image.resize((w * resize_factor, h * resize_factor), Image.ANTIALIAS)
+
     return image
+
