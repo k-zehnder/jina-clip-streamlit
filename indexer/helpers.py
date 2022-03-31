c7239b8ddda673ac584b05d60ec33ab3088c4f9d
Kevin Z <kjzehnder3@gmail.com>
2022-03-31 13:32:27 -0700

    checkpoint working search by image and text

--- indexer/helpers.py
+++ indexer/helpers.py
@@ -30,3 +30,10 @@ def resize_image(filename, resize_factor=2):
     w, h = image.size
     return image.resize((w * resize_factor, h * resize_factor), Image.ANTIALIAS)
 
+def get_embedded_da_from_img_files(images_path, num):
+    return DocumentArray.from_files(images_path, num).apply(
+        lambda d: d.load_uri_to_image_tensor()
+        .load_uri_to_image_tensor(200, 200)  # load
+        .set_image_tensor_normalization()  # normalize color
+        .set_image_tensor_channel_axis(-1, 0)  # switch color axis for the PyTorch model later
+    )    
