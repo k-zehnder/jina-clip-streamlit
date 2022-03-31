df29feedd39ff129d3926c7decbf143d3a671f14
Kevin Z <kjzehnder3@gmail.com>
2022-03-31 13:58:59 -0700

    cleanup zombie comments

--- image_searcher/app.py
+++ image_searcher/app.py
@@ -1,19 +1,11 @@
-import os
-from jina import Flow, Document, DocumentArray
-from jina import Flow, Document, DocumentArray, Executor, Flow, requests
-import torch
-from transformers import CLIPFeatureExtractor, CLIPModel, CLIPTokenizer
 from typing import Optional, Dict, List, Sequence
-from docarray import DocumentArray, Document
-from docarray.array.sqlite import SqliteConfig
-
-
 from copy import deepcopy
-from typing import Any, Dict, Optional, Sequence
+
+from jina import Flow, Document, DocumentArray, Executor, Flow, requests
 
 import torch
-from docarray import DocumentArray
-from jina import Executor, requests
+
+from transformers import CLIPFeatureExtractor, CLIPModel, CLIPTokenizer
 from transformers import CLIPFeatureExtractor, CLIPModel, CLIPTokenizer
 
 
