976bddfd54ad9459fb7a6602974145534e4a3e1b
Kevin Z <kjzehnder3@gmail.com>
2022-03-31 16:41:55 -0700

    more format standardizing

--- indexer/app.py
+++ indexer/app.py
@@ -206,6 +206,7 @@ class CLIPEncoder(Executor):
 IMAGES_PATH = "./data/tattoo_images/*.jpg"
 images = DocumentArray.from_files(IMAGES_PATH)
 
+
 current_dir = pathlib.Path(__file__).parent.resolve()
 if os.path.exists(os.path.join(current_dir, "workspace")):
     print("[INFO] removing existing workspace...")
