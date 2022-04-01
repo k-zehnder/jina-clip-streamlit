55b5ba16af224669a0e276a2110b5e969385ee32
Kevin Z <kjzehnder3@gmail.com>
2022-03-31 17:26:35 -0700

    readme credits

--- frontend/widgets.py
+++ frontend/widgets.py
@@ -18,6 +18,9 @@ def sidebar_widget():
     st.sidebar.markdown(
         "[Repo link](https://github.com/k-zehnder/jina-clip-streamlit)"
     )
+    st.sidebar.markdown(
+        "[Inspired by](https://github.com/jina-ai/example-multimodal-fashion-search)"
+    )
     return input_media
 
     
